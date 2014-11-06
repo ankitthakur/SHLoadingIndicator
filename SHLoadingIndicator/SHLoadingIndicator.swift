350657636d8be8bf6673f55805ce9d0ade719bc9
Ankit Thakur <ankitthakur85@gmail.com>
2014-11-06 17:06:56 +0530

    integrated 4 layers of animation

--- SHLoadingIndicator/SHLoadingIndicator.swift
+++ SHLoadingIndicator/SHLoadingIndicator.swift
@@ -13,14 +13,7 @@ import QuartzCore
 class SHLoadingIndicator:UIView {
     
     internal var borderColor:UIColor?;
-    
     internal var borderWidth:CGFloat?;
-    
-    internal var biggerShadeLayer:CAShapeLayer?;
-    internal var smallerShadeLayer:CAShapeLayer?;
-    
-    internal var biggerBezierPath:UIBezierPath?;
-    internal var smallerBezierPath:UIBezierPath?;
     
     internal var startAngle:CGFloat?;
     internal var endAngle:CGFloat?;
@@ -43,56 +36,71 @@ class SHLoadingIndicator:UIView {
         
         borderWidth = 2;
         
-        biggerShadeLayer = CAShapeLayer();
-        smallerShadeLayer = CAShapeLayer();
-        
-        biggerBezierPath = UIBezierPath();
-        smallerBezierPath = UIBezierPath();
         startAngle = CGFloat(M_PI_2);
         endAngle = CGFloat(2*M_PI);
         animate();
     }
     
-    internal func animate(){
-        
-        var center:CGPoint = CGPointMake(CGRectGetMidX(self.bounds), CGRectGetMidY(self.bounds));
-        var radius:CGFloat = min(self.bounds.size.width/5, self.bounds.size.height/5);
-        
-        var blueHalf:UIBezierPath = UIBezierPath(arcCenter: center, radius: CGFloat(radius), startAngle: startAngle!, endAngle: endAngle!, clockwise: true);
-        blueHalf.lineWidth = borderWidth!;
-
-        var radius1:CGFloat = min(self.bounds.size.width/3, self.bounds.size.height/3);
-        var blueHalf1:UIBezierPath = UIBezierPath(arcCenter: center, radius: CGFloat(radius1), startAngle: startAngle!, endAngle: endAngle!, clockwise: true);
-        blueHalf1.lineWidth = borderWidth!;
-
-        biggerShadeLayer!.frame = self.bounds;
-        smallerShadeLayer!.frame = self.bounds;
-        biggerShadeLayer!.strokeColor = borderColor!.CGColor;
-        smallerShadeLayer!.strokeColor = borderColor!.CGColor;
-        biggerShadeLayer!.lineWidth = borderWidth!;
-        smallerShadeLayer!.lineWidth = borderWidth!;
+    
+    internal func animate() {
         
         
+        var scale1:CGFloat = self.bounds.size.width * 0.35;
+        var scale2:CGFloat = self.bounds.size.width * 0.75;
+        var scale3:CGFloat = self.bounds.size.width * 0.85;
+        var scale4:CGFloat = self.bounds.size.width * 0.95;
         
-        biggerShadeLayer!.shadowColor = borderColor!.CGColor;
-        biggerShadeLayer!.shadowOffset = CGSizeMake(0.0, 0.0);
-        biggerShadeLayer!.shadowOpacity = 1;
-        biggerShadeLayer!.shadowRadius = radius;
+        var layerRect1:CGRect = CGRectMake(scale1 , scale1, self.bounds.size.width - 2*scale1,  self.bounds.size.height - 2*scale1);
+        var bezierPath1:UIBezierPath = UIBezierPath();
+        var shapeLayer1:CAShapeLayer =  CAShapeLayer();
+        bezierPath1.addArcWithCenter(CGPointMake(CGRectGetMidX(layerRect1), CGRectGetMidY(layerRect1)), radius: CGRectGetWidth(layerRect1) / 2, startAngle: startAngle!, endAngle: endAngle!, clockwise: true);
         
-        smallerShadeLayer!.shadowColor = borderColor!.CGColor;
-        smallerShadeLayer!.shadowOffset = CGSizeMake(0.0, 0.0);
-        smallerShadeLayer!.shadowOpacity = 1;
-        smallerShadeLayer!.shadowRadius = radius1;
+        var layerRect2:CGRect = CGRectMake(scale2 , scale2, self.bounds.size.width - 2*scale2,  self.bounds.size.height - 2*scale2);
+        var bezierPath2:UIBezierPath = UIBezierPath();
+        var shapeLayer2:CAShapeLayer =  CAShapeLayer();
+        bezierPath2.addArcWithCenter(CGPointMake(CGRectGetMidX(layerRect2), CGRectGetMidY(layerRect2)), radius: CGRectGetWidth(layerRect2) / 2, startAngle: startAngle!, endAngle: endAngle!, clockwise: true);
         
+        var layerRect3:CGRect = CGRectMake(scale3 , scale3, self.bounds.size.width - 2*scale3,  self.bounds.size.height - 2*scale3);
+        var bezierPath3:UIBezierPath = UIBezierPath();
+        var shapeLayer3:CAShapeLayer =  CAShapeLayer();
+        bezierPath3.addArcWithCenter(CGPointMake(CGRectGetMidX(layerRect3), CGRectGetMidY(layerRect3)), radius: CGRectGetWidth(layerRect3) / 2, startAngle: startAngle!, endAngle: endAngle!, clockwise: true);
         
-        biggerShadeLayer!.path = blueHalf.CGPath;
-        smallerShadeLayer!.path = blueHalf1.CGPath;
+        var layerRect4:CGRect = CGRectMake(scale4 , scale4, self.bounds.size.width - 2*scale4,  self.bounds.size.height - 2*scale4);
+        var bezierPath4:UIBezierPath = UIBezierPath();
+        var shapeLayer4:CAShapeLayer =  CAShapeLayer();
+        bezierPath4.addArcWithCenter(CGPointMake(CGRectGetMidX(layerRect4), CGRectGetMidY(layerRect4)), radius: CGRectGetWidth(layerRect4) / 2, startAngle: startAngle!, endAngle: endAngle!, clockwise: true);
         
-        self.layer.addSublayer(smallerShadeLayer);
-        self.layer.addSublayer(biggerShadeLayer);
-//        self.layer.addSublayer(biggerShadowLayer);
-//        self.layer.addSublayer(smallerShadowLayer);
-
+        bezierPath1.lineWidth = borderWidth!;
+        bezierPath2.lineWidth = borderWidth!;
+        bezierPath3.lineWidth = borderWidth!;
+        bezierPath4.lineWidth = borderWidth!;
+        
+        for shapeLayer in [shapeLayer1, shapeLayer2, shapeLayer3, shapeLayer4] {
+            
+            shapeLayer.frame = self.bounds;
+            shapeLayer.strokeColor = borderColor!.CGColor;
+            shapeLayer.lineWidth = borderWidth!;
+            shapeLayer.shadowColor = borderColor!.CGColor;
+            shapeLayer.shadowOffset = CGSizeMake(0.0, 0.0);
+            shapeLayer.shadowOpacity = 1;
+            
+        }
+        
+        shapeLayer1.shadowRadius = scale1;
+        shapeLayer2.shadowRadius = scale2;
+        shapeLayer3.shadowRadius = scale3;
+        shapeLayer4.shadowRadius = scale4;
+        
+        shapeLayer1.path = bezierPath1.CGPath;
+        shapeLayer2.path = bezierPath2.CGPath;
+        shapeLayer3.path = bezierPath3.CGPath;
+        shapeLayer4.path = bezierPath4.CGPath;
+        
+        self.layer.addSublayer(shapeLayer4);
+        self.layer.addSublayer(shapeLayer3);
+        self.layer.addSublayer(shapeLayer2);
+        self.layer.addSublayer(shapeLayer1);
+        
         var rotationAnimation1:CABasicAnimation = CABasicAnimation(keyPath: "transform.rotation.z");
         rotationAnimation1.fromValue = NSNumber(float: 0.0);
         rotationAnimation1.toValue = NSNumber(double: 2*M_PI);
@@ -105,14 +113,15 @@ class SHLoadingIndicator:UIView {
         rotationAnimation2.duration = 1.5;
         rotationAnimation2.repeatCount = Float.infinity;
         
-        
-        biggerShadeLayer!.addAnimation(rotationAnimation1, forKey: "indeterminateAnimation");
-        smallerShadeLayer!.addAnimation(rotationAnimation2, forKey: "indeterminateAnimation");
+        shapeLayer1.addAnimation(rotationAnimation1, forKey: "indeterminateAnimation");
+        shapeLayer2.addAnimation(rotationAnimation2, forKey: "indeterminateAnimation");
+        shapeLayer3.addAnimation(rotationAnimation1, forKey: "indeterminateAnimation");
+        shapeLayer4.addAnimation(rotationAnimation2, forKey: "indeterminateAnimation");
         
         self.layer.cornerRadius = self.bounds.size.width/2;
         self.layer.backgroundColor = UIColor.blackColor().CGColor;
         
-        
     }
+    
     
 }
